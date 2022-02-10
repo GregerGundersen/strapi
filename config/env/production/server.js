e64f6efd451daba7c5d62a1141d6f2eb52f416a7
GregerGundersen <gregergundersen@gmail.com>
2022-02-10 13:04:15 +0100

    Please work

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: env("your-app-url"),
+  url: env("https://git.heroku.com/workflow-strapiheroku.git"),
   proxy: true,
   app: { keys: env.array("APP_KEYS", ["yourkey1", "yourkey2"]) },
 });
